ae9763303438dc2ac82f81fe85da14e20ab6e18d
shruthinayak <rn.shruthi@gmail.com>
2016-09-17 19:04:38 -0700

    Code cleanup for code health :)

---
+++ app/src/main/java/hackday/com/carhack/Constants.java
@@ -0,0 +1,20 @@
+package hackday.com.carhack;
+
+/**
+ * Created by shruthi on 17/9/16.
+ */
+
+public final class Constants {
+    static String BASE_URL = "http://172.24.1.1:8080";
+
+    public static final class Cheats {
+        public static final String FORWARD_LEFT = "35,95,2.5,";
+        public static final String FORWARD_RIGHT = "35,95,2.5,";
+    }
+
+    public static final class EndPoints{
+        public static final String ACTION = "/action";
+        public static final String IMAGE = "/image";
+    }
+
+}
--- app/src/main/java/hackday/com/carhack/PlaceholderFragment.java
+++ app/src/main/java/hackday/com/carhack/PlaceholderFragment.java
@@ -10,7 +10,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,15 +20,13 @@ import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PlaceholderFragment extends Fragment {
+public class PlaceholderFragment extends Fragment implements View.OnClickListener {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -37,6 +34,17 @@ public class PlaceholderFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
     private RecyclerView mRecyclerView;
     private static ListAdapter mAdapter = new ListAdapter(new ArrayList<String>());
+    private TextView numberOfSecs;
+    private TextView txtLeftProgress;
+    private TextView txtRightProgress;
+    private Switch swit;
+    private SeekBar seekLeft;
+    private SeekBar seekRight;
+    private Button btnAdd;
+    private Button btnLeft;
+    private FloatingActionButton btnGo;
+    private ImageView btnCamera;
+    private Button btnRight;
 
     public PlaceholderFragment() {
     }
@@ -58,116 +66,12 @@ public class PlaceholderFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = null;
         if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-
             rootView = inflater.inflate(R.layout.fragment_main, container, false);
             getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-            final SeekBar seekLeft = (SeekBar) rootView.findViewById(R.id.seekLeft);
-            final SeekBar seekRight = (SeekBar) rootView.findViewById(R.id.seekRight);
-            final TextView numberOfSecs = (TextView) rootView.findViewById(R.id.txt_secs);
-            FloatingActionButton btnGo = (FloatingActionButton) rootView.findViewById(R.id.btn_go);
-            btnGo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ArrayList<String> s = new ArrayList<>();
-                    s.add("http://172.24.1.1:8080/action");
-                    for(String i: mAdapter.dataset){
-                        s.add(i);
-                    }
-                    new ServerAsyncTask().execute(s);
-                }
-            });
 
-            Button add = (Button)rootView.findViewById(R.id.btn_add);
-            final Switch swit = (Switch) rootView.findViewById(R.id.switch_obs);
+            initMainUI(rootView);
+            setUIListeners();
 
-
-            final TextView txtLeftProgress = (TextView) rootView.findViewById(R.id.txt_left_progress);
-            final TextView txtRightProgress = (TextView) rootView.findViewById(R.id.txt_right_progress);
-
-            add.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int left = Integer.parseInt(txtLeftProgress.getText().toString());
-                    int right = Integer.parseInt(txtRightProgress.getText().toString());
-                    float time;
-                    if(numberOfSecs.getText().toString().isEmpty())
-                        time = 0;
-                    else
-                        time = Float.parseFloat(numberOfSecs.getText().toString().split("s")[0]);
-                    String check_obs = getObstacle(swit);
-                    mAdapter.dataset.add(String.valueOf(left) + "," + String.valueOf(right) + "," + String.valueOf(time) + "," + check_obs);
-                    mAdapter.notifyDataSetChanged();
-
-                }
-            });
-
-            seekLeft.setMax(190);
-            seekRight.setMax(190);
-
-            seekLeft.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                    progress = progress / 10;
-                    progress = progress * 10;
-                    txtLeftProgress.setText(String.valueOf(progress-95));
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-
-            seekRight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                    progress = progress / 10;
-                    progress = progress * 10;
-                    txtRightProgress.setText(String.valueOf(progress-95));
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-            Button front_left = (Button) rootView.findViewById(R.id.btn_for_left);
-            front_left.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mAdapter.dataset.add("35,95,2.5,"+getObstacle(swit));
-                    mAdapter.notifyDataSetChanged();
-                }
-            });
-            Button front_right = (Button) rootView.findViewById(R.id.btn_for_right);
-            front_right.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mAdapter.dataset.add("99,29,2.5,"+getObstacle(swit));
-                    mAdapter.notifyDataSetChanged();
-                }
-            });
-
-            final ImageView camera = (ImageView) rootView.findViewById(R.id.img_camera_pic);
-            camera.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    /*ArrayList<String> n = new ArrayList<String>();
-                    n.add("http://172.24.1.1:8080/image");
-                    new ServerAsyncTask().execute(n);*/
-                    Picasso.with(getActivity()).load("http://172.24.1.1:8080/image").into(camera);
-                }
-            });
         } else {
             rootView = inflater.inflate(R.layout.fragment_items, container, false);
             mRecyclerView = (RecyclerView) rootView.findViewById(R.id.list_tuples);
@@ -185,12 +89,102 @@ public class PlaceholderFragment extends Fragment {
         return rootView;
     }
 
+    private void setUIListeners() {
+        btnGo.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnLeft.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+        btnCamera.setOnClickListener(this);
+
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                progress = progress / 10;
+                progress = progress * 10;
+                progress = progress - 95;
+                if (seekBar.getId() == R.id.seekLeft)
+                    txtLeftProgress.setText(String.valueOf(progress));
+                else
+                    txtRightProgress.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+        seekLeft.setOnSeekBarChangeListener(listener);
+        seekRight.setOnSeekBarChangeListener(listener);
+    }
+
+    private void initMainUI(View rootView) {
+        seekLeft = (SeekBar) rootView.findViewById(R.id.seekLeft);
+        seekRight = (SeekBar) rootView.findViewById(R.id.seekRight);
+        numberOfSecs = (TextView) rootView.findViewById(R.id.txt_secs);
+        btnAdd = (Button) rootView.findViewById(R.id.btn_add);
+        swit = (Switch) rootView.findViewById(R.id.switch_obs);
+        txtLeftProgress = (TextView) rootView.findViewById(R.id.txt_left_progress);
+        txtRightProgress = (TextView) rootView.findViewById(R.id.txt_right_progress);
+        btnLeft = (Button) rootView.findViewById(R.id.btn_for_left);
+        btnRight = (Button) rootView.findViewById(R.id.btn_for_right);
+        btnGo = (FloatingActionButton) rootView.findViewById(R.id.btn_go);
+        btnCamera = (ImageView) rootView.findViewById(R.id.img_camera_pic);
+
+        seekLeft.setMax(190);
+        seekRight.setMax(190);
+    }
+
+
     @NonNull
-    private String getObstacle(Switch swit) {
+    private String getObstacle() {
         String check_obs = "0";
         if (swit.isChecked()) {
             check_obs = "1";
         }
         return check_obs;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_add:
+                int left = Integer.parseInt(txtLeftProgress.getText().toString());
+                int right = Integer.parseInt(txtRightProgress.getText().toString());
+                float time;
+                if (numberOfSecs.getText().toString().isEmpty())
+                    time = 0;
+                else
+                    time = Float.parseFloat(numberOfSecs.getText().toString().split("s")[0]);
+                String check_obs = getObstacle();
+                mAdapter.dataset.add(String.valueOf(left) + "," + String.valueOf(right) + "," + String.valueOf(time) + "," + check_obs);
+                mAdapter.notifyDataSetChanged();
+                break;
+
+            case R.id.btn_go:
+                ArrayList<String> s = new ArrayList<>();
+                s.add(Constants.BASE_URL + Constants.EndPoints.ACTION);
+                for (String i : mAdapter.dataset) {
+                    s.add(i);
+                }
+                new ServerAsyncTask().execute(s);
+                break;
+
+            case R.id.btn_for_left:
+                mAdapter.dataset.add(Constants.Cheats.FORWARD_LEFT + getObstacle());
+                mAdapter.notifyDataSetChanged();
+                break;
+
+            case R.id.btn_for_right:
+                mAdapter.dataset.add(Constants.Cheats.FORWARD_RIGHT + getObstacle());
+                mAdapter.notifyDataSetChanged();
+                break;
+
+        }
     }
 }
